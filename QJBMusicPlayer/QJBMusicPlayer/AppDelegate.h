82d9d47ec2a25964026683a9bb4af67c0d4a6bb7
macbookpro <macbookpro@macbookprodeMacBook-Pro-3.local>
2016-12-24 19:00:34 +0800

    创建工程项目/ Creat Programme

---
+++ QJBMusicPlayer/QJBMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QJBMusicPlayer
+//
+//  Created by macbookpro on 16/12/24.
+//  Copyright © 2016年 macbookpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QJBMusicPlayer/QJBMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QJBMusicPlayer
+//
+//  Created by macbookpro on 16/12/24.
+//  Copyright © 2016年 macbookpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
